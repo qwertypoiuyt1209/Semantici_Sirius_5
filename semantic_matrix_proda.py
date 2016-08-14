e6abe164b8b26e552783d1b1a63eef214ef5f07f
NameSirname <potchtovy_jashik@mail.ru>
2016-08-15 01:07:16 +0300

    parallelize summation of matrix

--- semantic_matrix_proda.py
+++ semantic_matrix_proda.py
@@ -1,15 +1,20 @@
+import multiprocessing as mp
 import numpy as np
 import glob
 import os
+import sys
 
 
-input_dir = sys.argv[1]
-output_file = sys.argv[2]
-Matr=np.array()
-a,b=
-def complete_m(filename,Matr):
-    Matr.append(np.load(filename))
-    return Matr
+def complete_m(files):
+    matrix = None
+    for filename in files:
+        if None is matrix:
+            matrix = np.loadtxt(filename)
+        else:
+            matrix += np.loadtxt(filename)
+        print 'file {} processed'.format(filename)
+    return matrix
+
 
 def standardize_by_rows(Matr):
     mean_by_rows = np.mean(Matr, axis=0)
@@ -18,12 +23,33 @@ def standardize_by_rows(Matr):
     return out_matrix
 
 
-all_input_files=glob.glob(os.path.join(input_dir,'*'))
-for file in all_input_files:
-    Matr=complete_m(file,Matr)
-
-Matr=standardize_by_rows(Matr)
-np.savetxt(output_file,Matr)
+def chunkify(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 
+if __name__== '__main__':
+    input_dir = sys.argv[1]
+    output_file = sys.argv[2]
+    all_input_files = glob.glob(os.path.join(input_dir, '*'))
+    num_cores = mp.cpu_count()
+    chunk_size = len(all_input_files) / num_cores
+    chunks = list(chunkify(all_input_files, chunk_size))
 
+    pool = mp.Pool(processes=num_cores)
+    final_result = None
+    for i, result_matrix in enumerate(pool.imap_unordered(complete_m, chunks)):
+        print 'Part {} processed successfully'.format(i)
+        if None is final_result:
+            final_result = result_matrix
+        else:
+            final_result += result_matrix
+    pool.close()
+    pool.join()
+
+    # # Matr=np.log1p(Matr)
+    # # Matr = standardize_by_rows(Matr)
+    # # Matr = standardize_by_rows(Matr.T).T
+
+    np.savetxt(output_file, final_result, fmt='%.6f')
