cd0aae19b15c913af25713131b263d964b04f7a1
Имя Фамилия <potchtovy_jashik@mail.ru>
2016-08-15 14:59:07 +0300

    нор

--- semantic_matrix_finish.py
+++ semantic_matrix_finish.py
@@ -3,13 +3,17 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 matrix = np.load(input_file)
-def standardize_by_rows(Matr):
-    mean_by_rows = np.mean(Matr, axis=0)
-    std_by_rows = np.std(Matr, axis=0)
-    out_matrix = (Matr - mean_by_rows) / std_by_rows
+def standardize_by_strings(Matr):
+    mean_by_strings = np.mean(Matr, axis=1)
+    std_by_strings = np.std(Matr, axis=1)
+    out_matrix = np.array([])
+    for e in range(len(std_by_rows)):
+        if std_by_rows[e]!=0:
+            out_matrix.append((Matr[e] - mean_by_strings[e]) / std_by_strings[e])
     return out_matrix
 
-matrix=standardize_by_rows(matrix)
-matrix=standardize_by_rows(matrix.T).T
+matrix = np.log1p(matrix)
+matrix = standardize_by_strings(matrix)
+matrix = standardize_by_strings(matrix.T).T
 
 np.savetxt(output_file, matrix)
