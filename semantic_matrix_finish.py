b126dabdb9cdd2508156741ecd409358dcea8fb4
Имя Фамилия <potchtovy_jashik@mail.ru>
2016-08-15 15:00:06 +0300

    нор

--- semantic_matrix_finish.py
+++ semantic_matrix_finish.py
@@ -6,7 +6,7 @@ matrix = np.load(input_file)
 def standardize_by_strings(Matr):
     mean_by_strings = np.mean(Matr, axis=1)
     std_by_strings = np.std(Matr, axis=1)
-    out_matrix = np.array([])
+    out_matrix = []
     for e in range(len(std_by_rows)):
         if std_by_rows[e]!=0:
             out_matrix.append((Matr[e] - mean_by_strings[e]) / std_by_strings[e])
