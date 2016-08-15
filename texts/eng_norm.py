abb1b85422ec9b00092aaea4d117be12732ea2c8
Имя Фамилия <potchtovy_jashik@mail.ru>
2016-08-15 21:50:52 +0300

    нор

--- texts/eng_norm.py
+++ texts/eng_norm.py
@@ -8,6 +8,7 @@ input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 all_input_files = glob.glob(os.path.join(input_dir, '*'))
 
+FORBIDDEN_TAGS = ['IN','AT','TO','CS','CC','NUM']
 def normalize(in_filename, output_dir):
     with open(in_filename) as file:
         raw_text = [i.strip() for i in file.readlines()]
@@ -15,7 +16,7 @@ def normalize(in_filename, output_dir):
     for e in range(len(raw_text)):
         text.extend(re.split(r'\W+', raw_text[e]))
     wnl = nltk.WordNetLemmatizer()
-    text = [wnl.lemmatize(t.lower()) for t in text]
+    text = [wnl.lemmatize(t.lower()) for t in text if nltk.pos_tag(t.lower()) not in FORBIDDEN_TAGS]
 
     out_filename = os.path.join(output_dir, os.path.basename(in_filename))
     with open(out_filename,'w') as out_file:
